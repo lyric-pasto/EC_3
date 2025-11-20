aa4fef133742bcf1c7ad43bfcd69987cec731de6
Lylic²-Devrēx <yandelpomacarbajal043@gmail.com>
2025-11-19 22:50:42 -0500

    comentarios actualizados

--- Figonacci.js
+++ Figonacci.js
@@ -2,13 +2,10 @@
 const readline = require('readline').createInterface({
     input: process.stdin,
     output: process.stdout
-}); //Importando el módulo readline para que puede  acceder a la entrada 
-// y salida estándar mediante teclado y pantalla.
-// lee desde el teclado 
-// y escribe en la consola
+}); //Importando el módulo readline para poder ingresar datos por consola
 
-readline.question("Enter a number to find its Fibonacci value: ", (input) => {//pide al usuario que ingrese un número
-    const n = parseInt(input);//convierte la entrada en un número entero
+readline.question("Enter a number to find its Fibonacci value: ", (input) => {// nos pide ingresar numeros por teclado
+    const n = parseInt(input);//nos ayuda a convertir el valor ingresado por teclado a un número entero
 
     function fibonacci(num) {//función recursiva para calcular el valor de Fibonacci
         if (num <= 0) return 0;//caso base para 0
@@ -16,11 +13,11 @@ readline.question("Enter a number to find its Fibonacci value: ", (input) => {//
         return fibonacci(num - 1) + fibonacci(num - 2);//llamada recursiva
     }
 
-    if (isNaN(n) || n < 0) {//validación de entrada
-        console.log("Please enter a valid positive number.");//mensaje de error si la entrada no es válida
+    if (isNaN(n) || n < 0) {// nos ayuda a validar que el el numero ingresado por teclado sea un numero positivo
+        console.log("ingrese un numero positivo.");//mensaje de error si ingresamos un numero negativo
     } else {
         const result = fibonacci(n);
-        console.log(`Fibonacci value of ${n} is: ${result}`);//imprime el resultado
+        console.log(`el valor de fifonacci de ${n} es: ${result}`);//imprime el resultado por consola
     }
 
     readline.close();//cierra la interfaz de readline
